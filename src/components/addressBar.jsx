43fc777287fe33055a573582a4b03eb621f24677
Anshad.MK <84131708+Anshad-mk@users.noreply.github.com>
2023-09-17 03:45:43 +0530

    addressBar and NavBarCompleted Hero Started

---
+++ src/components/addressBar.jsx
@@ -0,0 +1,15 @@
+const addressBar = () => {
+  return (
+    <div className="h-8 bg-[#33A1B0] flex gap-4 text-white font-montserrat text-base font-medium leading-6 tracking-wider items-center justify-end pe-4">
+      <div>
+  Email: <a href="mailto:cozmekinfo@gmail.com">cozmekinfo@gmail.com</a>
+</div>
+<div>
+  Phone: <a href="tel:+919061401943">+919061401943</a>, <a href="tel:+917510747180">+917510747180</a>
+</div>
+     
+    </div>
+  )
+}
+
+export default addressBar
---
+++ src/components/heroSection.jsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+  return (
+    <div>
+      Hero section
+    </div>
+  )
+}
+
+export default HeroSection
