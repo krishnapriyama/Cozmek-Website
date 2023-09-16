9c7d5e94ca3f426665c4175ce61109c1093f7afd
Anshad.MK <84131708+Anshad-mk@users.noreply.github.com>
2023-09-17 05:09:49 +0530

    hero section left side completed

--- src/HomePage/home.jsx
+++ src/HomePage/home.jsx
@@ -1,18 +1,15 @@
-import HeroSection from "../components/HeroSection"
-import AddressBar from "../components/addressBar"
-import NavBar from "../components/navBar"
-
+import HeroSection from "../components/HeroSection";
+import AddressBar from "../components/addressBar";
+import NavBar from "../components/navBar";
 
 const Home = () => {
   return (
     <div>
-<AddressBar/>
-<NavBar/>
-<HeroSection/>
-
-
+      <AddressBar />
+      <NavBar />
+      <HeroSection />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
