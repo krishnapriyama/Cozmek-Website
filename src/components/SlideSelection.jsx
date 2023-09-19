e4a5032863be028e3eeabb75944d9ad2a491fe22
Anshad.MK <84131708+Anshad-mk@users.noreply.github.com>
2023-09-19 15:24:10 +0530

    slider width issue

--- src/components/SlideSelection.jsx
+++ src/components/SlideSelection.jsx
@@ -72,62 +72,69 @@ let slide = [
 ];
 const SlideSelection = () => {
   return (
-    <div className="flex-wrap w-auto h-auto px-3 py-4 gap-5 flex items-center justify-center ">
+    <div className="flex-wrap w-auto h-auto px-3 py-4 gap-5 flex items-center justify-center  my-5">
       {slide.map((card, index) => {
         return (
-          <div key={index} className="">
-            <div className=" max-w-[40rem] min-h-[650px]  container  bg-white rounded-xl overflow-hidden border-2  relative">
-              <div>
-                <img
-                  className="w-[40rem] h-[11.8rem]"
-                  src={card?.mainImage}
-                  alt="CardImage"
-                />
+          <div
+            key={index}
+            className=" max-w-[32rem] min-h-[650px]  container  bg-[#FAFAFA] rounded-xl overflow-hidden border-2  relative"
+            style={{
+              flexShrink: 0, // Prevent flex-shrinking
+            }}>
+            <div>
+              <img
+                className="w-[40rem] h-[11.8rem]"
+                src={card?.mainImage}
+                alt="CardImage"
+              />
+            </div>
+            <div
+              className="w-auto flex xs:w-[246.51px] items-center gap-5 absolute top-[11rem] left-8"
+              style={{
+                height: "41px",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                paddingTop: "5px",
+                paddingBottom: "5px",
+                background: "#FAFAFA",
+                borderRadius: "40px",
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}>
+              <div
+                className="relative w-auto xs:w-[91.01px]"
+                style={{ height: "31px" }}>
+                {card?.roundImages.map((img, index) => {
+                  return (
+                    <img
+                      key={index}
+                      className={`w-[29.19px] h-[29.19px]  top-0 left-[${
+                        card?.roundImages.length * 20 - index * 20
+                      }px]  absolute rounded-full border border-gray-400`}
+                      src={img}
+                    />
+                  );
+                })}
               </div>
               <div
-                className="w-auto flex xs:w-[246.51px] items-center gap-5 absolute top-[11rem] left-8"
-                style={{
-                  height: "41px",
-                  paddingLeft: "10px",
-                  paddingRight: "10px",
-                  paddingTop: "5px",
-                  paddingBottom: "5px",
-                  background: "#FAFAFA",
-                  borderRadius: "40px",
-                  justifyContent: "flex-start",
-                  alignItems: "center",
-                }}>
-                <div
-                  className="relative w-auto xs:w-[91.01px]"
-                  style={{ height: "31px" }}>
-                  {card?.roundImages.map((img, index) => {
-                    return (
-                      <img
-                        key={index}
-                        className={`w-[29.19px] h-[29.19px]  top-0 left-[${
-                          card?.roundImages.length * 20 - index * 20
-                        }px]  absolute rounded-full border border-gray-400`}
-                        src={img}
-                      />
-                    );
-                  })}
-                </div>
-                <div
-                  className="text-center text-gray-700 text-16 font-kanit font-normal leading-28 "
-                  style={{ wordWrap: "break-word" }}>
-                  + {card?.Project} Projects
-                </div>
+                className="text-center text-gray-700 text-16 font-kanit font-normal leading-28 "
+                style={{ wordWrap: "break-word" }}>
+                + {card?.Project} Projects
               </div>
+            </div>
 
-              <div className="p-4 pt-8">
-                <h4 className="font-sans text-[#9393b6] pb-4">{card?.type}</h4>
-                <h1 className="text-2xl text-[#0B7077] font-sans font-bold">
-                  {card?.MainHead}
-                </h1>
-                <p className="text-sm font-sans text-[#7b7b96] py-2">
-                  {card?.Discription}
-                </p>
+            <div className="p-4 pt-8 flex flex-col  ">
+              <h4 className="font-sans text-[#9393b6] pb-4">{card?.type}</h4>
 
+              <h1 className="text-2xl text-[#0B7077] font-sans font-bold ">
+                {card?.MainHead}
+              </h1>
+
+              <p className="text-sm font-sans text-[#7b7b96] py-2">
+                {card?.Discription}
+              </p>
+
+              <div className="">
                 <div className="flex items-center">
                   <img
                     src="/src/assets/ticMark.png"
@@ -193,17 +200,17 @@ const SlideSelection = () => {
                     {card?.Placement}
                   </h6>
                 </div>
+              </div>
 
-                <div className="flex items-center justify-between pt-2">
-                  <div className="flex gap-3 font-extrabold">
-                    <h2 className="text-red-500">₹ {card?.DiscountAmount}K</h2>
-                    <h2 className="line-through">₹ {card?.OriginalAmount}K</h2>
-                  </div>
-
-                  <button className="bg-[#0B7077] p-2 rounded-md text-white capitalize">
-                    enroll now
-                  </button>
+              <div className="flex items-center justify-between pt-2">
+                <div className="flex gap-3 font-extrabold">
+                  <h2 className="text-red-500">₹ {card?.DiscountAmount}K</h2>
+                  <h2 className="line-through">₹ {card?.OriginalAmount}K</h2>
                 </div>
+
+                <button className="bg-[#0B7077] p-2 rounded-md text-white capitalize">
+                  enroll now
+                </button>
               </div>
             </div>
           </div>
